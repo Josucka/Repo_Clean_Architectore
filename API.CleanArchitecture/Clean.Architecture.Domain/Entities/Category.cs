a0787de86a468d297ef83bf9c40af6185057f187
Josué Barros <frajolla27@gmail.com>
2022-07-12 17:31:45 -0300

    <feat>(create entities, interface, domainException)

---
+++ API.CleanArchitecture/Clean.Architecture.Domain/Entities/Category.cs
@@ -0,0 +1,38 @@
+﻿using Clean.Architecture.Domain.Validation;
+using System.Collections.Generic;
+
+namespace Clean.Architecture.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When( id < 0, $"Id Invalid");
+            Id = id;
+            ValidationDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), $" Invalid {name}");
+
+            DomainExceptionValidation.When(name.Length < 3, $" Invalid {name}");
+
+            Name = name;
+        }
+    }
+}
---
+++ API.CleanArchitecture/Clean.Architecture.Domain/Entities/Product.cs
@@ -0,0 +1,55 @@
+﻿using Clean.Architecture.Domain.Validation;
+
+namespace Clean.Architecture.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public int CategoryId { get; set; }
+        public Category Categorys { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidationDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Id Invalid");
+            Id = id;
+            ValidationDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidationDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidationDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), $" Invalid {name}");
+            DomainExceptionValidation.When(name.Length < 3, $" Invalid {name}");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), $" Invalid {description}");
+            DomainExceptionValidation.When(description.Length < 5, $" Invalid {description}");
+            
+            DomainExceptionValidation.When(price < 0, $" Invalid {price}");
+            
+            DomainExceptionValidation.When(stock < 0, $" Invalid {stock}");
+            
+            DomainExceptionValidation.When(image.Length < 250, $" Invalid {image}");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+    }
+}
