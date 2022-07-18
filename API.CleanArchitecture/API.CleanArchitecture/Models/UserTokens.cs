3af2870a5dc7f87f5b12c0263b18a4b9684fa2d4
Josué Barros <frajolla27@gmail.com>
2022-07-18 11:05:34 -0300

    <feat>(implement generate Token no project API)

---
+++ API.CleanArchitecture/API.CleanArchitecture/Models/UserTokens.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Clean.Architecture.Api.Models
+{
+    public class UserTokens
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
